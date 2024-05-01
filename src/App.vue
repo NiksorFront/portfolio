7c1d1e5118d3ad43ede9db94dfd926f28c5d1e68
Niksor <Salfordyoutube@ya.ru>
2024-05-01 23:45:00 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<script setup>
+
+</script>
+
+<template>
+  <header>
+    <h1 class="title"> Порфолио Николая Сорокина</h1>
+  </header>
+
+  <main>
+    <p class="text">В 14 лет начал изучать Python. В 15 моделирование в Blender 3D, дизайн и геймдев. В 16 занимался робототехникой и соответствующим программированием. Сейчас мне 19 и уже почти год занимаюсь фронтенд разработкой. 
+    Сайты, свёрстанные и написанные мною на ванильном JavaScript: 
+    <a class="url" href="https://niksorfront.github.io/russian-travel-bootcamp/"> https://niksorfront.github.io/russian-travel-bootcamp/</a><br/>
+    <a class="url" href="https://niksorfront.github.io/mesto-project-bootcamp/"> https://niksorfront.github.io/mesto-project-bootcamp/</a><br/>
+    <a class="url" href="https://vladislav-spiridonov.ru/"> https://vladislav-spiridonov.ru/</a><br/>
+    <a class="url"  href="https://niksorok.github.io/Soalla-website/"> https://niksorok.github.io/Soalla-website/</a><br/>
+    </p>
+
+    <p class="text">Тут JavaScript + React:<br/>
+    <a class="url"  href="https://soalla.ru/"> https://soalla.ru/ </a><br/>
+    <a class="url"  href=" https://krenol.ru/"> https://krenol.ru/</a> (Сейчас в разработке, поэтому пока без адаптива)</p>
+
+    <p class="text">Тут TypeScript + React:<br/><a class="url"  href="https://niksorfront.github.io/react-stellar-burger/"> https://niksorfront.github.io/react-stellar-burger/</a></p>
+
+    <p class="text">Docker'ом пользоваться умею. И сейчас изучаю Vue 3. На нём уже написал два сайта:<br/>
+    <a class="url"  href=" hukcopff.beget.tech"> hukcopff.beget.tech</a><br/>
+    <a class="url"  href="https://beautiful-lego.ru"> https://beautiful-lego.ru </a></p>
+
+    <p class="text">Другие мои работы- <a class="url"  href=""> https://disk.yandex.ru/d/1LJ9raS5HB_aoQ</a></p>
+  </main>
+</template>
+
+<style scoped>
+</style>
